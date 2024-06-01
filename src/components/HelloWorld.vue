dd3a5b0e177998d3800e7c311537cfafc5431559
zkriszti <zavecz.krisztina@gmail.com>
2024-06-01 19:52:50 +0200

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+  <div class="card">
+  <button type="button" @click="count--">-</button>
+  <div :class="{'colored': count > 3}" class="box">count is {{ count }}</div>
+  <button type="button" @click="count++">+</button>
+  </div>
+
+  
+</template>
+
+<style scoped>
+.card {
+  display: flex; 
+  gap: 8px;
+  justify-content: center;
+}
+.read-the-docs {
+  color: #888;
+}
+.box {
+  display: flex;
+  align-items: center;
+  padding: 0 12px;
+}
+.colored {
+  background: wheat;
+}
+</style>
