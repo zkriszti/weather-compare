38ef26e42263bc7b0173a74c687afd3cc92880ec
Krisztina Závecz <krisztina.zavecz@memoq.com>
2025-01-05 17:34:17 +0100

    add graphics to header

--- src/components/WeatherCompareHeader.vue
+++ src/components/WeatherCompareHeader.vue
@@ -1,12 +1,37 @@
 <script setup></script>
 
 <template>
-  <h1 class="weather-compare-header">weather-compare</h1>
+  <div class="header-wrapper">
+    <header>
+      <h1 class="weather-compare-header">weather-compare</h1>
+      <img src="/weather_icons/2.png" alt="" />
+    </header>
+  </div>
 </template>
 
 <style scoped>
-h1 {
+header {
   align-self: center;
+  position: relative;
+  display: inline-flex;
+}
+h1 {
   font-family: "Rammetto One", sans-serif;
+}
+
+img {
+  position: absolute;
+  right: -22px;
+  top: 24px;
+  z-index: -1;
+  clip-path: polygon(
+    55% 100%,
+    100% 100%,
+    100% 0%,
+    0% 0%,
+    0% 45%,
+    55% 45%,
+    55% 100%
+  );
 }
 </style>
